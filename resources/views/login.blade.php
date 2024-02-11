2fbcc4ce4fcab9b9c61e4ee8d013315c03a52a57
Hendraghz <hendraghz79@gmail.com>
2024-02-11 13:56:43 +0700

    ADD Folder Awal

---
+++ resources/views/login.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Login Page</title>
+</head>
+
+<body class="bg-gray-100 dark:bg-gray-900">
+    <div class="flex justify-center items-center h-screen">
+        <div>
+            <div class="flex justify-center items-center mb-5">
+                <img src="{{ asset('images/RW-ku (1).jpg') }}" alt="rwku" width="80">
+            </div>
+            <div class="text-center mb-20">
+                <h3 class="text-3xl  dark:text-white">Selamat Datang</h3>
+                <h4 class="text-2xl font-bold dark:text-white">Sistem Informasi Buku Penghubung</h4>
+            </div>
+
+            <div class="card">
+                <div
+                    class="w-full max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <form class="max-w-full mx-auto">
+                        <div class="mb-5">
+                            <label for="username"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                            <input type="text" id="username"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="username" required>
+                        </div>
+                        <div class="mb-5">
+                            <label for="password"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                password</label>
+                            <input type="password" id="password"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required placeholder="*********">
+                        </div>
+                        <div class="flex justify-center">
+                            <button type="submit"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
